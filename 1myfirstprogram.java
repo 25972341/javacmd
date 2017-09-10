9f28d8b85a4d06e599bccf9c71947bcbcb39e248
25972341 <manohar.dutt@gmail.com>
2017-09-10 20:25:05 +0530

    add all files

---
+++ 1myfirstprogram.java
@@ -0,0 +1,5 @@
+class Car2{
+public static void main(String[] args){
+System.out.println("I am car2");	
+}
+}
---
+++ carVariablesDemo.java
@@ -0,0 +1,30 @@
+public class carVariablesDemo {
+		static byte speed=100;
+		static int cost=800000;
+		static float mileage;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		char version;
+		boolean isPetrol=true;
+		String name="Honda";
+		
+		
+		
+		System.out.println("speed:"+speed);
+		System.out.println("cost:"+cost);
+		System.out.println("mileage:"+mileage);
+
+		System.out.println("isPetrol:"+isPetrol);
+		System.out.println("name:"+name);
+		
+		System.out.println("*****RoughWork****");
+		System.out.println(90);
+		System.out.println(90+90);
+		System.out.println("90"+90);
+		System.out.println("90"+90+90);
+		System.out.println("90"+(90+90));
+	}
+
+}
